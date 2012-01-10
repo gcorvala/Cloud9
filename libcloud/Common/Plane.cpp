b4f50954f0ae014dea78dc9de51276617accec11
Gabriel Corvalan <just.the.boss@gmail.com>
2012-01-10 16:50:37 +0100

    Implement constructors in Plane

--- libcloud/Common/Plane.cpp
+++ libcloud/Common/Plane.cpp
@@ -5,19 +5,43 @@ Plane::Plane (const Point& p, const Vector& normal)
   a = normal.x;
   b = normal.y;
   c = normal.z;
-  Vector p0 = p;
-  d = -(normal.dot(p0));
+  Vector v = p;
+  d = -normal.dot(v);
 }
 
-Plane::Plane (const Vector& v1, const Vector& v2)
+Plane::Plane (const Vector& v1, const Vector& v2, const Point& p)
 {
+  Vector normal = v1.cross (v2);
+
+  a = normal.x;
+  b = normal.y;
+  c = normal.z;
+
+  Vector v = p;
+
+  d = -normal.dot (v);
 }
 
 Plane::Plane (const Point& p1, const Point& p2, const Point& p3)
 {
+  Vector v1 = Vector (p2)-Vector (p1);
+  Vector v2 = Vector (p3)-Vector (p1);
+  Vector normal = v1.cross (v2);
+
+  a = normal.x;
+  b = normal.y;
+  c = normal.z;
+
+  Vector v = p1;
+
+  d = -normal.dot (v);
 }
 
-Plane::Plane (double a, double b, double c, double d)
+Plane::Plane (double _a, double _b, double _c, double _d)
+  :a(_a)
+  ,b(_b)
+  ,c(_c)
+  ,d(_d)
 {
 }
 
@@ -30,6 +54,8 @@ Vector
 Plane::getNormal () const
 {
   Vector normal;
+
+  // FIXME : error if a,b or c = 0
 
   Vector p1 (-d/a, 0, 0);
   Vector p2 (0, -d/b, 0);
--- libcloud/Common/Plane.h
+++ libcloud/Common/Plane.h
@@ -7,9 +7,9 @@
 class Plane {
   public:
     Plane (const Point& p, const Vector& normal);
-    Plane (const Vector& v1, const Vector& v2);
+    Plane (const Vector& v1, const Vector& v2, const Point& p);
     Plane (const Point& p1, const Point& p2, const Point& p3);
-    Plane (double a, double b, double c, double d);
+    Plane (double _a = 0, double _b = 0, double _c = 0, double _d = 0);
     virtual ~Plane ();
 
     Vector getNormal () const;
