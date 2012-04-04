683a4aaf4727a816ccf038a2e6667b44dbdbd6ae
Gabriel Corvalan <just.the.boss@gmail.com>
2012-04-04 12:16:49 +0200

    dwg: update

--- src/dwg/dwg.cpp
+++ src/dwg/dwg.cpp
@@ -1,7 +1,20 @@
-#include <dwg.h>
+#include <OdaCommon.h>
+#include <Ge/GePlane.h>
+#include <iostream>
 
 int
 main (int argc, char** argv)
 {
+  OdGePlane plane;
+  OdGePoint3d uPoint(1.0, 1.0, 1.0), origin(0.0, 0.0, 0.0), vPoint(1.0, 1.0, -1.0);
+  double a, b, c, d;
+  OdGePoint3d point1, point2, point3;
+
+  plane.set(uPoint, origin, vPoint);
+  plane.get(point1, point2, point3);
+  plane.getCoefficients(a, b, c, d);
+
+  std::cout << "a: " << a << " b: " << b << " c: " << c << " d: " << d << std::endl;
+
   return 0;
 }
